906ddab68dc8b478cf5d133006c387b0e6749eaf
Numan Korkmaz <heysemkorkmaz@gmail.com>
2021-04-06 14:07:34 +0200

    init car & ultrasound sensor

--- arduino/smartcar/smartcar.ino
+++ arduino/smartcar/smartcar.ino
@@ -1,9 +1,57 @@
 #include <Smartcar.h>
+#include <GP2D120.hpp>
 
-void setup() {
-  // put your setup code here, to run once:
+const unsigned long PRINT_INTERVAL = 100;
+unsigned long previousPrintout     = 0;
+
+ArduinoRuntime arduinoRuntime;
+BrushedMotor leftMotor(arduinoRuntime, smartcarlib::pins::v2::leftMotorPins);
+BrushedMotor rightMotor(arduinoRuntime, smartcarlib::pins::v2::rightMotorPins);
+DifferentialControl control(leftMotor, rightMotor);
+
+GY50 gyroscope(arduinoRuntime, 37);
+
+const auto pulsesPerMeter = 600;
+
+DirectionlessOdometer leftOdometer{
+    arduinoRuntime,
+    smartcarlib::pins::v2::leftOdometerPin,
+    []() { leftOdometer.update(); },
+    pulsesPerMeter};
+DirectionlessOdometer rightOdometer{
+    arduinoRuntime,
+    smartcarlib::pins::v2::rightOdometerPin,
+    []() { rightOdometer.update(); },
+    pulsesPerMeter};
+
+
+
+SmartCar car(arduinoRuntime, control, gyroscope, leftOdometer, rightOdometer);
+
+const auto triggerPin = 6;
+const auto echoPin = 7;
+const auto maxDistance = 400;
+const unsigned short IR_PIN = A1;
+GP2D120 infraredSensor(arduinoRuntime, IR_PIN);
+SR04 front(arduinoRuntime, triggerPin, echoPin, maxDistance);
+
+void setup()
+{
+    Serial.begin(9600);
+
+    car.enableCruiseControl();
+    car.setSpeed(1.5); // Maintain a speed of 1.5 m/sec
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+    // Maintain the speed and update the heading
+    car.update();
+    unsigned long currentTime = millis();
+    if (currentTime >= previousPrintout + PRINT_INTERVAL)
+    {
+        previousPrintout = currentTime;
+        Serial.println(infraredSensor.getMedianDistance());
+    }
+    
 }
