c4eb39b2e356e5101d3607b2f06da59d38ff52d1
jeens <jeens93@gmail.com>
2021-04-14 15:53:34 +0200

    Create initial layout for application GUI
    Refs #15

--- client/app/src/main/java/group01/smartcar/client/MainActivity.java
+++ client/app/src/main/java/group01/smartcar/client/MainActivity.java
@@ -2,15 +2,16 @@ package group01.smartcar.client;
 
 import android.os.Bundle;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
-import android.view.View;
+import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
