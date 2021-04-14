df39a9c1a8a1c38ee15a8e7012312415d125dd31
Mislav Milicevic <mislav.milicevic22@gmail.com>
2021-04-14 15:16:45 +0200

    Convert Android project to Java

---
+++ client/app/src/main/java/group01/smartcar/client/MainActivity.java
@@ -0,0 +1,20 @@
+package group01.smartcar.client;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
