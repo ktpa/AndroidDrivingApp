b9329c78f2a15e66ad882e02ef56672f346f6c23
Mislav Milicevic <mislav.milicevic22@gmail.com>
2021-04-14 16:11:00 +0200

    Add component callbacks
    Refs #15

--- client/app/src/main/java/group01/smartcar/client/MainActivity.java
+++ client/app/src/main/java/group01/smartcar/client/MainActivity.java
@@ -1,6 +1,7 @@
 package group01.smartcar.client;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 import org.eclipse.paho.client.mqttv3.IMqttActionListener;
@@ -15,5 +16,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        registerComponentCallbacks();
+    }
+
+    private void registerComponentCallbacks() {
+        findViewById(R.id.start_button).setOnClickListener(this::onStartClick);
+        findViewById(R.id.stop_button).setOnClickListener(this::onStopClick);
+        findViewById(R.id.leftDir_button).setOnClickListener(this::onLeftDirClick);
+        findViewById(R.id.rightDir_button).setOnClickListener(this::onRightDirClick);
+    }
+
+    private void onStartClick(View view) {
+        // TODO
+    }
+
+    private void onStopClick(View view) {
+        // TODO
+    }
+
+    private void onLeftDirClick(View view) {
+        // TODO
+    }
+
+    private void onRightDirClick(View view) {
+        // TODO
     }
 }
