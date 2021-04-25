56fd2e0308ce23bb4e65e0ba49b1bc9e9fe1c75f
gsastraw <geraldi@net.chalmers.se>
2021-04-25 11:52:23 +0200

    Clean up unnecessary imports and comments

--- client/app/src/main/java/group01/smartcar/client/MainActivity.java
+++ client/app/src/main/java/group01/smartcar/client/MainActivity.java
@@ -6,29 +6,17 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.VideoView;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-import static group01.smartcar.client.Status.*;
-
 public class MainActivity extends AppCompatActivity {
-
-    // TODO: Login button only transitions to next screen, account authentication has not been implemented
-
     private FirebaseAuth firebaseAuth;
     EditText emailTextView, passwordTextView;
     private VideoView videoBackground;
