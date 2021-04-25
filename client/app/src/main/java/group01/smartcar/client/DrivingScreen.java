2a6e099cab30303cfec29c88afaa719c53659472
Karl <itkalle@outlook.com>
2021-04-25 11:23:13 +0200

    Hidden the status bar in driving mode, refactoring buttons for clarity. #27

--- client/app/src/main/java/group01/smartcar/client/DrivingScreen.java
+++ client/app/src/main/java/group01/smartcar/client/DrivingScreen.java
@@ -13,13 +13,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import static group01.smartcar.client.Status.ACTIVE;
 
+// 70 to 103 adapted from https://developer.android.com/training/system-ui/immersive .
+
 public class DrivingScreen extends AppCompatActivity implements JoystickView.JoystickListener {
     CarControl car;
     protected ImageView cameraView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        Log.d("Test", "DrivingScreen.onCreate: Reached!");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drive);
         registerComponentCallbacks();
@@ -42,7 +43,6 @@ public class DrivingScreen extends AppCompatActivity implements JoystickView.Joy
     }
 
     private void registerComponentCallbacks() {
-        Log.d("Test", "DrivingScreen.registerComponentCallbacks: Reached");
         findViewById(R.id.start_button).setOnClickListener(this::onStartClick);
         findViewById(R.id.stop_button).setOnClickListener(this::onStopClick);
     }
@@ -65,4 +65,41 @@ public class DrivingScreen extends AppCompatActivity implements JoystickView.Joy
             car.throttle(speed);
         }
     }
+
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            hideSystemUI();
+        }
+    }
+
+    private void hideSystemUI() {
+        // Enables regular immersive mode.
+        // For "lean back" mode, remove SYSTEM_UI_FLAG_IMMERSIVE.
+        // Or for "sticky immersive," replace it with SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE
+                        // Set the content to appear under the system bars so that the
+                        // content doesn't resize when the system bars hide and show.
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        // Hide the nav bar and status bar
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN);
+    }
+
+    // Shows the system bars by removing all the flags
+// except for the ones that make the content appear under the system bars.
+    private void showSystemUI() {
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+    }
+
 }
